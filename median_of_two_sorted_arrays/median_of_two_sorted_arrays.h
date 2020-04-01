645bc046f8dae24d9f4e98e4feeaebc246b0cca2
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-04-01 07:12:04 -0700

    Removed unsolved problem

--- median_of_two_sorted_arrays/median_of_two_sorted_arrays.h
+++
@@ -1,9 +0,0 @@
-#ifndef MEDIAN_OF_TWO_SORTED_ARRAYS_H
-#define MEDIAN_OF_TWO_SORTED_ARRAYS_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
-double findMedianSortedArrays(int *, int, int *, int);
-
-#endif /* MEDIAN_OF_TWO_SORTED_ARRAYS_H */
